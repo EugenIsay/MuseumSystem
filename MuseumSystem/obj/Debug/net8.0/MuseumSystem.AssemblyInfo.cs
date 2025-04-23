cee3b077bb68e457cbffcdf6d2a5885d2c7a9933
EugenIsay <zheka4311@gmail.com>
2025-04-23 16:08:15 +0300

    0.0.1

--- MuseumSystem/obj/Debug/net8.0/MuseumSystem.AssemblyInfo.cs
+++ MuseumSystem/obj/Debug/net8.0/MuseumSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuseumSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7caa14996c3ebb07b0f11474770b1078ecdab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuseumSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuseumSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
