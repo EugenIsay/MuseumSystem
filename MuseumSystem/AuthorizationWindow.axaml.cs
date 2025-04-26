643e968abc818d103585f9bf1218ffc87a53a003
EugenIsay <zheka4311@gmail.com>
2025-04-26 20:05:50 +0300

    0.0.2

--- MuseumSystem/AuthorizationWindow.axaml.cs
+++ MuseumSystem/AuthorizationWindow.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using MsBox.Avalonia;
+using System;
 
 namespace MuseumSystem;
 
@@ -9,5 +12,49 @@ public partial class AuthorizationWindow : Window
     public AuthorizationWindow()
     {
         InitializeComponent();
+        BDay.DisplayDateEnd = System.DateTime.Now.AddYears(-12);
+        Gender.ItemsSource = Helper.Genders;
+    }
+
+    private void Login_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (Helper.IsExist(FirstRow.Text, Password.Text))
+        {
+            
+        }
+    }
+
+    private void Change_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        RightTop.IsVisible = !RightTop.IsVisible;
+        LeftTop.IsVisible = !LeftTop.IsVisible;
+        
+    }
+
+    private void Register_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (RegPassword.Text != RegPasswordCheck.Text)
+        {
+            Helper.CallMessageBox("Проверьте правильность пароля и попробуйте заново" ,this);
+        }
+        if (!DateOnly.TryParse(BDay.Text, out DateOnly result))
+        {
+            Helper.CallMessageBox("Укажите дату рождения в формате день месяц год через точки", this);
+            return;
+        }
+        if (Helper.CanRegister(new Models.User() { FirstName = FirstName.Text,
+            LastName = LastName.Text,
+            Patronymic = Patronymic.Text,
+            Email = Email.Text,
+            Login = Login.Text,
+            PhoneNumber = Phone.Text,
+            GenderId = (Gender.SelectedItem as Models.Gender).Id,
+            Birthday = DateOnly.Parse(BDay.Text),
+            Password = Password.Text,
+            RoleId = 3
+        }, this))
+        {
+
+        }
     }
 }
---
+++ MuseumSystem/Helper.cs
@@ -0,0 +1,124 @@
+﻿using Avalonia.Controls;
+using MsBox.Avalonia;
+using MuseumSystem.Context;
+using MuseumSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumSystem
+{
+    public static class Helper
+    {
+        static User3Context DBContext = new User3Context();
+        static User currentUser = null;
+        public static List<Gender> Genders
+        {
+            get => DBContext.Genders.ToList();
+        }
+        public static List<User> Users
+        {
+            get => DBContext.Users.ToList();
+        }
+
+        public static bool IsExist(string FirsRow, string Password)
+        {
+            currentUser = DBContext.Users.FirstOrDefault(u => (u.Login == FirsRow || u.Email == FirsRow) && u.Password == Password);
+            return currentUser != null;
+        }
+        public static bool CanRegister(User User, AuthorizationWindow Window)
+        {
+            if (string.IsNullOrEmpty(User.Login))
+            {
+                CallMessageBox("Придумайте себе логин", Window);
+                return false;
+            }
+            if (DBContext.Users.Select(u => u.Login).Contains(User.Login))
+            {
+                CallMessageBox("Данный логин уже занят", Window);
+                return false;
+            }
+            if (string.IsNullOrEmpty(User.FirstName) || string.IsNullOrEmpty(User.LastName))
+            {
+                CallMessageBox("Укажите имя и фамилию", Window);
+                return false;
+            }
+            if (string.IsNullOrEmpty(User.Email) || !IsValidEmail(User.Email) || string.IsNullOrEmpty(User.PhoneNumber) || User.PhoneNumber.Contains("_"))
+            {
+                CallMessageBox("Укажите правильные контактные данные", Window);
+                return false;
+            }
+            if (DBContext.Users.Select(u => u.Email).Contains(User.Email))
+            {
+                CallMessageBox("Данная почта уже занята", Window);
+                return false;
+            }
+            if (User.Birthday == null || User.Birthday > DateOnly.FromDateTime(DateTime.Now.AddYears(-12)))
+            {
+                CallMessageBox("Укажите свою дату рождения", Window);
+                return false;
+            }
+            if (User.GenderId == 0)
+            {
+                CallMessageBox("Укажите свой пол", Window);
+                return false;
+            }
+            if (string.IsNullOrEmpty(User.Password) || User.Password.Count() < 4)
+            {
+                CallMessageBox("Подберите более надёжный пароль", Window);
+                return false;
+            }
+            try
+            {
+                if (User.Id == 0)
+                {
+                    User.Id = DBContext.Users.Select(s => s.Id).Order().Last() + 1;
+                    DBContext.Users.Add(User);
+                }
+                else
+                {
+                    DBContext.Users.Update(User);
+                }
+                DBContext.SaveChanges();
+                currentUser = User;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        static bool IsValidEmail(string email)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+            {
+                return false;
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static void CallMessageBox(string Message, Window Window)
+        {
+            MessageBoxManager.GetMessageBoxStandard("Ошибка", Message).ShowWindowDialogAsync(Window);
+        }
+        public static bool IsAdmin
+        {
+            get => currentUser.RoleId == 1;
+        }
+        public static bool IsEmployee
+        {
+            get => currentUser.RoleId != 3;
+        }
+    }
+}
